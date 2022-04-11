61546173fc26d32ff23521d3f6079d3465f4cfcc
Harshal Jamdar <jamdarharshal@gmail.com>
2022-04-11 19:05:28 +0530

    mongoDB connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 
-const route = require('./routes/route.js');
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+mongoose
+  .connect(
+    "mongodb+srv://Hemant:ABUStLZdeMkI4nqS@cluster0.rp8ya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(console.log("connected to mongodb"));
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
